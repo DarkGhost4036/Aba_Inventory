02919c752c56ff8333c7130f401618f2b5254b0f
LENOVO_LOQ\aless <totopepe248@gmail.com>
2026-04-12 22:10:13 -0600

    ya funciona RegistroView

--- Tienda_Abarrotes/model/UserModel.cs
+++ Tienda_Abarrotes/model/UserModel.cs
@@ -14,6 +14,7 @@ namespace Tienda_Abarrotes.Model
         public string UserName { get; set; }
         public string Password { get; set; }
         public string Name { get; set; }
+        public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
         public string ConfirmPassword { get; set; }
